b0091d42a8ab099c082239c569c56b9a4ef91c4d
andrewdempsey2018 <andrewdempsey2016@gmail.com>
2024-04-14 11:47:24 +0100

    Quote added

--- src/main/java/ie/andrew/consume/ConsumeApplication.java
+++ src/main/java/ie/andrew/consume/ConsumeApplication.java
@@ -1,6 +1,7 @@
 package ie.andrew.consume;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import ie.andrew.consume.model.Quote;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -10,6 +11,8 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class ConsumeApplication {
@@ -38,16 +41,26 @@ public class ConsumeApplication {
 					"http://localhost:8080/api", JsonNode.class);
 			log.info(quote.toString());*/
 
-			JsonNode quote = restTemplate.getForObject(
-					"https://jsonplaceholder.typicode.com/posts/1", JsonNode.class);
-			log.info(quote.toString());
+			ArrayList<Quote> quotes = new ArrayList<>();
 
-			System.out.println(quote.getNodeType());
-			quote.forEach(q->{
-				System.out.println(q.get("title"));
+			quotes.add(restTemplate.getForObject(
+					"https://jsonplaceholder.typicode.com/posts/1", Quote.class));
+
+			quotes.add(restTemplate.getForObject(
+					"https://jsonplaceholder.typicode.com/posts/2", Quote.class));
+
+			quotes.add(restTemplate.getForObject(
+					"https://jsonplaceholder.typicode.com/posts/3", Quote.class));
+
+			quotes.add(restTemplate.getForObject(
+					"https://jsonplaceholder.typicode.com/posts/4", Quote.class));
+
+
+			quotes.forEach(quote->{
+				System.out.println(quote.getId());
+				System.out.println("***");
 			});
 
-			//https://jsonplaceholder.typicode.com/
 		};
 	}
 }
